307248334b487247cc4ff8ba1cf6b277f5200f21
Ivan <ivan.trashchenko@gmail.com>
2018-07-03 00:16:34 +0300

    Add Task 2

---
+++ IntegerGcdExtensions.NUnitTests/IntegerGcdExtensionNUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace IntegerGcdExtensions.NUnitTests
+{
+    [TestFixture]
+    public class IntegerGcdExtensionNUnitTests
+    {
+        [TestCase(2, 4, ExpectedResult = 2)]
+        [TestCase(13, 13, 13, ExpectedResult = 13)]
+        [TestCase(20, 100, 20, ExpectedResult = 20)]
+        [TestCase(37, 600, 1, ExpectedResult = 1)]
+        [TestCase(0, 624129, 2061517, 18913, ExpectedResult = 18913)]
+        [TestCase(16, 52, new int[] { 18, 48 }, ExpectedResult = 2)]
+        public int EuclidGcd_Test(int numberFirst, int numberSecond, params int[] array)
+        {
+            Debug.WriteLine($"Time elapsed: {IntegerGcdExtension.GetEuclidGcdTime(numberFirst, numberSecond, array)}.");
+            return IntegerGcdExtension.EuclidGcd(numberFirst, numberSecond, array);
+        }
+
+        [TestCase(2, 4, -2)]
+        public void EuclidGcd_ThrowsArgumentOutOfRangeException(int numberFirst, int numberSecond, params int[] array)
+        {    
+            Assert.Throws<ArgumentOutOfRangeException>(() => IntegerGcdExtension.EuclidGcd(numberFirst, numberSecond, array));
+        }
+
+        [TestCase(2, 4, ExpectedResult = 2)]
+        [TestCase(13, 13, 13, ExpectedResult = 13)]
+        [TestCase(20, 100, 20, ExpectedResult = 20)]
+        [TestCase(37, 600, 1, ExpectedResult = 1)]
+        [TestCase(0, 624129, 2061517, 18913, ExpectedResult = 18913)]
+        [TestCase(16, 52, new int[] { 18, 48 }, ExpectedResult = 2)]
+        public int SteinGcd_Test(int numberFirst, int numberSecond, params int[] array)
+        {
+            Debug.WriteLine($"Time elapsed: {IntegerGcdExtension.GetSteinGcdTime(numberFirst, numberSecond, array)}.");
+            return IntegerGcdExtension.SteinGcd(numberFirst, numberSecond, array);
+        }
+
+        [TestCase(2, -4)]
+        public void SteinGcd_ThrowsArgumentOutOfRangeException(int numberFirst, int numberSecond, params int[] array)
+        {        
+            Assert.Throws<ArgumentOutOfRangeException>(() => IntegerGcdExtension.SteinGcd(numberFirst, numberSecond, array));
+        }
+    }
+}
---
+++ IntegerGcdExtensions/IntegerGcdExtension.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace IntegerGcdExtensions
+{
+    /// <summary>
+    /// Class of methods for computing GCD.
+    /// </summary>
+    public static class IntegerGcdExtension
+    {
+        /// <summary>
+        /// Method which computes GCD of two numbers using Euclid's algorithm.
+        /// </summary>
+        /// <param name="numberFirst">First number.</param>
+        /// <param name="numberSecond">Second number.</param>
+        /// <returns>GCD of source numbers.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when source numbers are out of range.</exception>
+        public static int EuclidGcd(int numberFirst, int numberSecond)
+        {
+            if (numberFirst < 0)
+            {
+                throw new ArgumentOutOfRangeException($"{numberFirst} is out of range.");
+            }
+
+            if (numberSecond < 0)
+            {
+                throw new ArgumentOutOfRangeException($"{numberSecond} is out of range.");
+            }
+
+            if (numberFirst == numberSecond)
+            {
+                return numberFirst;
+            }
+
+            if (numberFirst == 0)
+            {
+                return numberSecond;
+            }
+
+            if (numberSecond == 0)
+            {
+                return numberFirst;
+            }
+
+            while (numberFirst != numberSecond)
+            {
+                if (numberFirst > numberSecond)
+                {
+                    numberFirst = numberFirst - numberSecond;
+                }
+                else
+                {
+                    numberSecond = numberSecond - numberFirst;
+                }
+            }
+
+            return numberFirst;
+        }
+
+        /// <summary>
+        /// Method which computes GCD of optional amount of numbers using Euclid's algorithm.
+        /// </summary>
+        /// <param name="numberFirst">First number.</param>
+        /// <param name="numberSecond">Second number.</param>
+        /// <param name="array">Optional numbers.</param>
+        /// <returns>GCD of source numbers.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when source numbers are out of range.</exception>
+        public static int EuclidGcd(int numberFirst, int numberSecond, params int[] array)
+        {
+            int gcd = EuclidGcd(numberFirst, numberSecond);
+
+            if (array.Length == 0)
+            {
+                return gcd;
+            }
+
+            Queue<int> queue = new Queue<int>(array);
+
+            return EuclidGcd(gcd, queue.Dequeue(), queue.ToArray());
+        }
+
+        /// <summary>
+        /// Method which finds the time Euclid's method takes to compute GCD of specicfic numbers.
+        /// </summary>
+        /// <param name="numberFirst">First number.</param>
+        /// <param name="numberSecond">Second number.</param>
+        /// <param name="array">Optional numbers.</param>
+        /// <returns>Elapsed time of type (TimeSpan).</returns>
+        public static TimeSpan GetEuclidGcdTime(int numberFirst, int numberSecond, params int[] array)
+        {
+            Stopwatch watch = Stopwatch.StartNew();
+            EuclidGcd(numberFirst, numberSecond, array);
+            watch.Stop();
+            return watch.Elapsed;
+        }
+
+        /// <summary>
+        /// Method which computes GCD of two numbers using Stein's algorithm.
+        /// </summary>
+        /// <param name="numberFirst">First number.</param>
+        /// <param name="numberSecond">Second number.</param>
+        /// <returns>GCD of source numbers.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when source numbers are out of range.</exception>
+        public static int SteinGcd(int numberFirst, int numberSecond)
+        {
+            if (numberFirst < 0)
+            {
+                throw new ArgumentOutOfRangeException($"{numberFirst} is out of range.");
+            }
+
+            if (numberSecond < 0)
+            {
+                throw new ArgumentOutOfRangeException($"{numberSecond} is out of range.");
+            }
+
+            if (numberFirst == numberSecond)
+            {
+                return numberFirst;
+            }
+
+            if (numberFirst == 0)
+            {
+                return numberSecond;
+            }
+
+            if (numberSecond == 0)
+            {
+                return numberFirst;
+            }
+
+            if ((~numberFirst & 1) != 0)
+            {
+                if ((numberSecond & 1) != 0)
+                {
+                    return SteinGcd(numberFirst >> 1, numberSecond);
+                }
+
+                return SteinGcd(numberFirst >> 1, numberSecond >> 1) << 1;
+            }
+
+            if ((~numberSecond & 1) != 0)
+            {
+                return SteinGcd(numberFirst, numberSecond >> 1);
+            }
+
+            if (numberFirst > numberSecond)
+            {
+                return SteinGcd((numberFirst - numberSecond) >> 1, numberSecond);
+            }
+
+            return SteinGcd((numberSecond - numberFirst) >> 1, numberFirst);
+        }
+
+        /// <summary>
+        /// Method which computes GCD of optional amount of numbers using Stein's algorithm.
+        /// </summary>
+        /// <param name="numberFirst">First number.</param>
+        /// <param name="numberSecond">Second number.</param>
+        /// <param name="array">Optional numbers.</param>
+        /// <returns>GCD of source numbers.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when source numbers are out of range.</exception>
+        public static int SteinGcd(int numberFirst, int numberSecond, params int[] array)
+        {
+            int gcd = SteinGcd(numberFirst, numberSecond);
+
+            if (array.Length == 0)
+            {
+                return gcd;
+            }
+
+            Queue<int> queue = new Queue<int>(array);
+
+            return SteinGcd(gcd, queue.Dequeue(), queue.ToArray());
+        }
+
+        /// <summary>
+        /// Method which finds the time Stein's method takes to compute GCD of specicfic numbers.
+        /// </summary>
+        /// <param name="numberFirst">First number.</param>
+        /// <param name="numberSecond">Second number.</param>
+        /// <param name="array">Optional numbers.</param>
+        /// <returns>Elapsed time of type (TimeSpan).</returns>
+        public static TimeSpan GetSteinGcdTime(int numberFirst, int numberSecond, params int[] array)
+        {
+            Stopwatch watch = Stopwatch.StartNew();
+            SteinGcd(numberFirst, numberSecond, array);
+            watch.Stop();
+            return watch.Elapsed;
+        }
+    }
+}
